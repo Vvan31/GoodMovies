ae8543b51ad970366b65b77cc3ae2190ba03205d
Viviana Osorio <v31.osorio@gmail.com>
2023-05-21 23:46:49 -0700

    Movie Reviews controller and DAO

--- src/Controller/api/movies.route.js
+++ src/Controller/api/movies.route.js
@@ -7,9 +7,16 @@ const router = express.Router() // get access to express router
 
 /* router.route('/').get((req,res) => res.send('hello world'))  */
 router.route('/').get((req,res) => res.send(MoviesController.apiGetMovies))
+//This route retrieves a specific movie and all reviews associated with that movie
+router.route("/id/:id").get(MoviesController.apiGetMovieById) 
+// This route returns a list of movie ratings (such as G, PG, R) so that a user 
+// can select the ratings from a dropdown menu in the frontend.
+router.route("/ratings").get(MoviesController.apiGetRatings)
+
+
+
 router.route("/review") 
   .post(ReviewsController.apiPostReview) 
   .put(ReviewsController.apiUpdateReview) 
   .delete(ReviewsController.apiDeleteReview)   
-
 module.exports = router // export router
